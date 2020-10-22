e9f32afd482a5f7ac5debf22adbcdb180a618581
Damodar Lohani <dlohani48@gmail.com>
2020-10-22 16:30:38 +0545

    basic layout and multi page

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Nav from '$components/Nav.svelte';
+</script>
+
+<main>
+	<Nav />
+	<slot></slot>
+</main>
+
+<style>
+	:root {
+		font-family: Quicksand, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,24 +2,12 @@
 	import Counter from '$components/Counter.svelte';
 </script>
 
-<main>
-	<h1>Hello world!</h1>
+	<h1>Hello Svelte!</h1>
 
 	<Counter/>
 	<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-	}
-
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
